038703247fee5c7b89d91bc74ee58407c1da7201
Pushpendra8318 <pushpandrarathour5@gmail.com>
2026-02-13 23:40:33 +0530

    Fix sameSite attribute value for cookie

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -28,7 +28,7 @@ export const registerUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: none,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
